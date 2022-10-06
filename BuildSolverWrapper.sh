0535c16ef19913b56ad79ed441e05cb87842b455
orhaimwerthaim <orhaimwerthaim@users.noreply.github.com>
2022-10-06 11:40:40 +0300

    Update BuildSolverWrapper.sh

--- BuildSolverWrapper.sh
+++ BuildSolverWrapper.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-/opt/cmake-3.19.8-Linux-x86_64/bin/cmake --build /home/or/Projects/AOS-Solver/build --config Release --target all -j 14 --
+#/opt/cmake-3.19.8-Linux-x86_64/bin/cmake --build ~/Projects/AOS-Solver/build --config Release --target all -j 14 --
+
+
+cmake --build ~/AOS/AOS-Solver/build --config Release --target all -j 14 --
